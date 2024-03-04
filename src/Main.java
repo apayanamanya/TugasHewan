0b893d8e462a3b0b35cec186cc554d85905225d5
apayanamanya <meutiarahmadhani225@gmail.com>
2024-03-04 14:41:04 +0700

    first

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    String nama = "Kiyowo";
+    int umur = 10;
+    String warna = "hitam dan putih";
+    String tinggi = "60-90 cm";
+    String berat = "70-120 kg";
+    String makanan = "1. Bambu, 2. Telur, 3. burung";
+
+    public static void main(String[] args) {
+        Main test = new Main();
+        System.out.println("Nama: " + test.nama);
+        System.out.println("Umur: " + test.umur);
+        System.out.println("Warna: " + test.warna);
+        System.out.println("Tinggi: " + test.tinggi);
+        System.out.println("Berat: " + test.berat);
+        System.out.println("Makanan: " + test.makanan);
+    }
+}
